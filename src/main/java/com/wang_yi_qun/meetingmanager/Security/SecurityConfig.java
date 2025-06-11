a673b9d9c9cdd3c089336787dc9d7b1619f617be
skyloved <w2783858980@outlook.com>
2025-06-11 20:31:47 +0800

    优化登录失败和成功处理逻辑，统一JSON响应格式，确保字符编码为UTF-8 没有禁用默认登入页面

--- src/main/java/com/wang_yi_qun/meetingmanager/Security/SecurityConfig.java
+++ src/main/java/com/wang_yi_qun/meetingmanager/Security/SecurityConfig.java
@@ -4,20 +4,14 @@ import com.wang_yi_qun.meetingmanager.Model.Employee;
 import com.wang_yi_qun.meetingmanager.Repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.DefaultLoginPageConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Optional;
 
 @Configuration
 @AllArgsConstructor
